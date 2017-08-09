9e6c947cb8f2dfe7cf7c4f824dcc2e4da6ced09e
Billy Vong <billyvg@gmail.com>
2017-08-09 11:29:32 -0700

    Fix storybook template, add `withInfo`

--- generators/component/templates/storybook.js
+++ generators/component/templates/storybook.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
+import {withInfo} from '@storybook/addon-info';
 // import {action} from '@storybook/addon-actions';
 
 import <%= name %> from 'sentry-ui/<%= fileName %>';
 
-storiesOf('<%= name %>', module).withInfo('default', '', () => (
+storiesOf('<%= name %>', module).add('default', withInfo('Description')(() => (
   <<%= name %> />
-));
+)));
